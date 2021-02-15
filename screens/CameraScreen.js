bba6cd7302a33cbe9da3698617a8b3e37fbec138
John Valle <vallejohn.vj@gmail.com>
2021-02-15 21:06:52 +0800

    Updated grid sizes, removed unused code

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import axios from "axios"
-import { StyleSheet, Text, View, Alert, Dimensions, ActivityIndicator, TextInput, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Alert, Dimensions, ActivityIndicator} from 'react-native';
 import * as ImageManipulator from 'expo-image-manipulator';
 import { Camera } from "expo-camera"
-import { Button, Input } from "react-native-elements";
+import { Button } from "react-native-elements";
 import Theme from '../Theme';
 
 export default function CameraScreen({ navigation }) {
   const [hasPermission, setHasPermission] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
-  const [showInput, setShowInput] = React.useState(false);
-  const [valueToShow, setValueToShow] = React.useState("");
   const cameraRef = React.useRef();
 
-  const textRef = React.useRef(valueToShow);
 
   React.useEffect(() => {
     (async () => {
@@ -36,13 +33,6 @@ export default function CameraScreen({ navigation }) {
           onPress={() => takePicture()}
           containerStyle={{ marginHorizontal: 12 }}
         />
-      ),
-      headerTitle: () => (
-        <>
-          <Pressable onPress={() => setShowInput(prevState => !prevState)}>
-            <Text>Camera</Text>
-          </Pressable>
-        </>
       )
     })
   }, [navigation])
@@ -70,13 +60,8 @@ export default function CameraScreen({ navigation }) {
             compress: 0.5
           }
         )
-        
+
         translate(manipulatedImg.base64)
-        // setIsLoading(true);
-        // setTimeout(() => {
-        //   setIsLoading(false);
-        //   Alert.alert(textRef.current);
-        // }, 4500)
       }
     }
   }
@@ -112,26 +97,15 @@ export default function CameraScreen({ navigation }) {
   return (
     <View style={styles.container}>
       {isLoading ? <ActivityIndicator size="large" color={Theme.colors.primaryLight} /> : (
-        <>
-          <Input
-            placeholder='input here'
-            onChangeText={(text) => {
-              setValueToShow(text);
-              textRef.current = text
-            }}
-            containerStyle={{ marginTop: 50, display: showInput ? "flex" : "none" }}
-            value={valueToShow}
-          />
-          <Camera ref={cameraRef} style={styles.camera} type={Camera.Constants.Type.back}>
-            <View style={{ display: "flex", flexDirection: "row" }}>
-              {[...generatePlaceholderArray(28)].map((_, idx) => (
-                <View key={idx}>
-                  {[...generatePlaceholderArray(7)].map((_, idx) => <View key={idx} style={styles.grid} />)}
-                </View>
-              ))}
-            </View>
-          </Camera>
-        </>
+        <Camera ref={cameraRef} style={styles.camera} type={Camera.Constants.Type.back}>
+          <View style={{ display: "flex", flexDirection: "row" }}>
+            {[...generatePlaceholderArray(28)].map((_, idx) => (
+              <View key={idx}>
+                {[...generatePlaceholderArray(10)].map((_, idx) => <View key={idx} style={styles.grid} />)}
+              </View>
+            ))}
+          </View>
+        </Camera>
       )}
     </View>
   );
@@ -152,6 +126,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "white",
     width: Dimensions.get("window").width / 28,
-    height: 360 / 8
+    height: 360 / 12
   }
 });
